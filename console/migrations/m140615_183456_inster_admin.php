f9111480945d5b18c3d51483cf10d5bd64d3fc48
Arinoid <quiver.diqu@gmail.com>
2014-06-21 11:54:05 +0300

    initial commit

---
+++ console/migrations/m140615_183456_inster_admin.php
@@ -0,0 +1,23 @@
+<?php
+
+use yii\db\Schema;
+
+class m140615_183456_inster_admin extends \yii\db\Migration
+{
+    public function up()
+    {
+        $this->insert('{{%user}}', [
+            'id' => 1,
+            'username' => 'admin',
+            'password_hash' => '$2y$13$L/4IJYUVHmeiSz7pg9COh.6oTrhQR7.gr85hFRVZzS9ihNAmDzi8W'
+        ]);
+
+    }
+
+    public function down()
+    {
+        $this->delete('{{%user}}', [
+            'id' => 1
+        ]);
+    }
+}
---
+++ frontend/views/site/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @var yii\web\View $this
+ */
+$this->title = 'Arinoid | Shorten URL';
+?>
+<div class="site-index">
+    <form class="form-inline text-center">
+        <div class="form-group">
+            <label class="sr-only" for="link">URL</label>
+            <input type="url" class="form-control" size="50" id="link" placeholder="Enter link">
+        </div>
+        <button type="submit" class="btn btn-default">Shorten URL</button>
+    </form>
+</div>
